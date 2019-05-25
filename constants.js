81b4e5a0fa7b6419012d9eeb749ec00dfc0b3b8d
KongBorup <avborup@gmail.com>
2019-05-25 09:42:35 +0200

    Celestial body class

--- constants.js
+++ constants.js
@@ -1,6 +1,9 @@
 // Masses are measured in Earth masses
 const SUN_MASS = 332946; // https://en.wikipedia.org/wiki/Solar_mass
 const EARTH_MASS = 1;
+// const SPACE_SHUTTLE_MASS = 0.01; // according to Google conversion from 2030 tonnes
+const SPACE_SHUTTLE_MASS = 3.39908811e-19; // according to Google conversion from 2030 tonnes
+const JUPITER_MASS = 317.8;
 
 // Universal gravitational constant rescaled from m^3 kg^-1 s^-2 to
 // AU^3 M⊕^-1 a^-2 (in order: astronomical units, earth masses, years)
@@ -8,5 +11,5 @@ const G = 1.185651e-4;
 
 // Options
 const dt = 0.0001;
-const numSteps = 3 / dt;
+const numSteps = 6 / dt;
 const animSpeed = 100;
--- sketch.js
+++ sketch.js
@@ -1,36 +1,70 @@
-let points;
+let earthPoints;
+let rocketPoints;
 let curIndex = 0;
 
 function setup() {
   createCanvas(800, 600);
-  points = calculateTrajectory();  
+  const { earth, rocket } = calculateTrajectory();  
+  earthPoints = earth;  
+  rocketPoints = rocket;
 }
 
 function calculateTrajectory() {
-  const positions = [];
-  const earth = {
+  const earthPositions = [];
+  const earth = new CelestialBody({
     pos: createVector(1, 0),
     vel: createVector(0, TWO_PI),
-  };
+    mass: EARTH_MASS,
+  });
+  const rocketPositions = [];
+  const rocket = new CelestialBody({
+    pos: createVector(1.001, -0.1),
+    vel: createVector(0, 0),
+    mass: SPACE_SHUTTLE_MASS,
+  });
+  const sun = new CelestialBody({
+    pos: createVector(0, 0),
+    vel: createVector(0, 0),
+    mass: SUN_MASS,
+  });
   
-  positions.push(earth.pos);
+  earthPositions.push(earth.pos);
+  rocketPositions.push(rocket.pos);
 
   for (let i = 0; i < numSteps; i++) {
-    const { vel, pos } = earth;
-    const { mult, add } = p5.Vector;
+    earth.update([sun, rocket]);
+    rocket.update([sun, earth]);
 
-    // Calculate acceleration using Newton's law of gravitation
-    const unitVector = pos.copy();
-    unitVector.normalize();
-    const acc = mult(unitVector, -G * SUN_MASS * EARTH_MASS / pos.mag() ** 2);
-    // Update velocity and position using Euler's method
-    earth.vel = add(vel, mult(acc, dt));
-    earth.pos = add(pos, mult(vel, dt));
+    // const { mult, add } = p5.Vector;
+
+    // // Calculate acceleration using Newton's law of gravitation
+    // const earthUnitVector = earth.pos.copy();
+    // earthUnitVector.normalize();
+    // const earthAcc = mult(earthUnitVector, -G * SUN_MASS * EARTH_MASS / earth.pos.mag() ** 2);
+
+    // const rocketUnitVector = rocket.pos.copy();
+    // rocketUnitVector.normalize();
+    // let rocketAcc = mult(rocketUnitVector, -G * SUN_MASS * SPACE_SHUTTLE_MASS / rocket.pos.mag() ** 2);
+
+    // const earthRocketAcc = mult(rocketUnitVector, -G * EARTH_MASS * SPACE_SHUTTLE_MASS / rocket.pos.dist(earth.pos) ** 2);
+
+    // rocketAcc = add(rocketAcc, earthRocketAcc);
+    // // Update velocity and position using Euler's method
+    // earth.pos = add(earth.pos, mult(earth.vel, dt));
+    // earth.vel = add(earth.vel, mult(earthAcc, dt));
+
+    // // Update velocity and position using Euler's method
+    // rocket.pos = add(rocket.pos, mult(rocket.vel, dt));
+    // rocket.vel = add(rocket.vel, mult(rocketAcc, dt));
     
-    positions.push(earth.pos);
+    earthPositions.push(earth.pos);
+    rocketPositions.push(rocket.pos);
   }
 
-  return positions;
+  return {
+    earth: earthPositions,
+    rocket: rocketPositions,
+  };
 }
 
 function draw() {
@@ -38,12 +72,20 @@ function draw() {
   translate(width / 2, height / 2);
   scale(100);
   strokeWeight(0.01);
-  stroke(34, 139, 34);
 
+  stroke(34, 139, 34);
   noFill();
-  beginShape();
+  beginShape(LINES);
   for (let i = 0; i <= curIndex; i += animSpeed) {
-    const { x, y } = points[i];
+    const { x, y } = earthPoints[i];
+    vertex(x, y);
+  }
+  endShape();
+  
+  stroke(255);
+  beginShape(LINES);
+  for (let i = 0; i <= curIndex; i += animSpeed) {
+    const { x, y } = rocketPoints[i];
     vertex(x, y);
   }
   endShape();
@@ -52,25 +94,32 @@ function draw() {
   fill(218, 165, 32);
   circle(0, 0, 0.5);
   
-  const { x, y } = points[curIndex];
+  const earthX = earthPoints[curIndex].x;
+  const earthY = earthPoints[curIndex].y;
+  const rocketX = rocketPoints[curIndex].x;
+  const rocketY = rocketPoints[curIndex].y;
+  
   fill(34, 139, 34);
-  circle(x, y, 0.1);
+  circle(earthX, earthY, 0.1);
+  fill(255);
+  circle(rocketX, rocketY, 0.05);
   
   scale(0.01);
   textSize(12);
-
   textAlign(CENTER, CENTER);
   fill(218, 165, 32);
   text('Sun', 0, 40);
-  
-  textAlign(CENTER, LEFT);
   fill(34, 139, 34);
-  text('Earth', x * 100 + 25, y * 100);
+  text('Earth', earthX * 100 + 25, earthY * 100);
+  fill(255);
+  text('Rocket', rocketX * 100 + 25, rocketY * 100);
 
   curIndex += animSpeed;
 
-  if (curIndex >= points.length) {
+  if (curIndex >= earthPoints.length) {
     console.log('FINISHED ANIMATING');
     noLoop();
+
+    console.log(rocketPoints[0], rocketPoints[rocketPoints.length - 1]);
   }
 }
